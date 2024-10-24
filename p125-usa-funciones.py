d978069ee6605bea9b0745c603f42faef01da647
BrandonCarrillo13 <brandoncarrillo840@gmail.com>
2024-10-24 08:49:01 -0600

    Actividad 16

---
+++ p125-usa-funciones.py
@@ -0,0 +1,14 @@
+##p120funciones 
+import p124funciones as pf
+
+l1 = pf.leer()
+
+l1 = pf.leer()
+may = pf.mayor(l1)
+men = pf.menor(l1)
+sum = pf.suma(l1)
+pro = pf.promedio(l1)
+
+print("La lista es >", l1)
+print("El mayor es", may, "El menor es", men)
+print("La suma es", sum, "El promedio es", pro)
